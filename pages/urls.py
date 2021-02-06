717d3bae3656433f2a7ad1560a8cc45d2da22e14
xacrys <cristiantintinmedina@hotmail.com>
2021-02-05 21:02:10 -0500

    CTI - Creación de CRUD

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from .views import PageListView, PageDetailView
+from .views import (
+    PageListView,
+    PageDetailView,
+    PageCreateView,
+    PageUpdateView,
+    PageDeleteView,
+)
 
 
 pathPages = (
@@ -8,6 +14,10 @@ pathPages = (
         path(
             "<int:pk>/<slug:slug>/", PageDetailView.as_view(), name="page"
         ),  # Utilizar la clave primaria no olvidar utilizar el pk
+        path("create", PageCreateView.as_view(), name="create"),
+        path("update/<int:pk>", PageUpdateView.as_view(), name="update"),
+        path("delete/<int:pk>", PageDeleteView.as_view(), name="delete"),
     ],
     "pages",
 )
+
--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Page
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -19,6 +21,26 @@ class PageListView(ListView):
 
 class PageDetailView(DetailView):
     model = Page
+
+
+class PageCreateView(CreateView):
+    model = Page
+    fields = ["title", "content", "order"]
+    success_url = reverse_lazy("pages:pages")
+
+
+class PageUpdateView(UpdateView):
+    model = Page
+    fields = ["title", "order"]
+    template_name_suffix = "_update_form"
+
+    def get_success_url(self):
+        return reverse_lazy("pages:update", args=[self.object.id]) + "?ok"
+
+
+class PageDeleteView(DeleteView):
+    model = Page
+    success_url = reverse_lazy("pages:pages")
 
 
 # def page(request, page_id, page_slug):
